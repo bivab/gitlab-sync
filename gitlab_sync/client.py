fd3175e4d0ec244696d949b902076c2cb08380d5
David Schneider <david.schneider@bivab.de>
2015-10-23 10:49:44 +0200

    Bump version: 0.1.2 → 0.1.3-dev

--- gitlab_sync/client.py
+++ gitlab_sync/client.py
@@ -12,7 +12,7 @@ logger = getLogger()
 
 DEFAULT_REMOTE = 'gitlab'
 
-__VERSION__ = '0.1.2'
+__VERSION__ = '0.1.3-dev'
 
 class ProgressIndicator(git.objects.submodule.base.UpdateProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(base, 'README.md')) as rm:
     readme = rm.read()
 
 setup(name="gitlab-sync",
-    version='0.1.2',
+    version='0.1.3-dev',
     author='David Schneider',
     author_email='david.schneider@bivab.de',
     url='https://github.com/bivab/gitlab-sync',
